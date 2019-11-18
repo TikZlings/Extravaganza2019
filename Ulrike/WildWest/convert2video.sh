6d798496ebfc82ad676a0708570fff89dfed27a7
samcarter <sam.carter8@aol.com>
2019-11-18 18:10:27 +0100

    First test for converting Wild West Marmot to video

---
+++ Ulrike/WildWest/convert2video.sh
@@ -0,0 +1,18 @@
+# clean up old video
+rm wildwest.mp4
+
+# convert to png images
+convert -density 160 wildwest.pdf wildwest.png
+
+# convert to video
+ffmpeg -ss 00:00:00 -i wildwest-%d.png -ss 00:01:30 -i Mundharmonika.m4a -shortest wildwest_raw.mp4
+
+# repair video
+HandBrakeCLI --crop 0:0:0:0  -i wildwest_raw.mp4 -o wildwest.mp4
+
+# clean up
+#rm wildwest-*.png
+rm wildwest_raw.mp4
+
+# view :)
+open wildwest.mp4
