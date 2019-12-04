50c9af3502587eacca21cf2c298d60beb740e008
samcarter <sam.carter8@aol.com>
2019-12-04 14:55:12 +0100

    adjusting duration and speed of bella Italia video

--- Carla/Bella_Italia/convert2video.sh
+++ Carla/Bella_Italia/convert2video.sh
@@ -2,7 +2,7 @@
 rm BellaItalia.mp4
 
 # convert to png images
-convert -density 160 BellaItalia_combine.pdf BellaItalia.png
+convert -density 160 BellaItalia.pdf BellaItalia.png
 
 # convert to video
 ffmpeg -ss 00:00:00 -i BellaItalia-%d.png -ss 00:01:25 -i UnEstateItaliana.m4a -shortest BellaItalia_raw.mp4
@@ -15,4 +15,4 @@ rm BellaItalia-*.png
 rm BellaItalia_raw.mp4
 
 # view :)
-open BellaItalia.mp4
+#open BellaItalia.mp4
