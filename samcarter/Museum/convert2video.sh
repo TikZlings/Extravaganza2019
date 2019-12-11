675d69c7cc0f114a19ef2d2287b83b198bf78fcf
samcarter <sam.carter8@aol.com>
2019-12-11 14:27:19 +0100

    added sound to Museum video

--- samcarter/Museum/convert2video.sh
+++ samcarter/Museum/convert2video.sh
@@ -5,15 +5,14 @@ rm Museum.mp4
 convert -density 160 Museum.pdf Museum.png
 
 # convert to video
-ffmpeg -ss 00:00:00 -i Museum-%d.png Museum_raw.mp4
-#ffmpeg -ss 00:00:00 -i Museum-%d.png -ss 00:00:00 -i Memory.m4a -shortest Museum_raw.mp4
+ffmpeg -ss 00:00:00 -i Museum-%d.png -ss 00:00:50 -i PicturesAtAnExhibition.m4a -shortest Museum_raw.mp4
 
 # repair video
 HandBrakeCLI --crop 0:0:0:0  -i Museum_raw.mp4 -o Museum.mp4
 
 # clean up
-#rm Museum-*.png
+rm Museum-*.png
 rm Museum_raw.mp4
 
 # view :)
-open Museum.mp4
+#open Museum.mp4
